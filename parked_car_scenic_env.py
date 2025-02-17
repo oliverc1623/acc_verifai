dd7fbd2f2b400b7d77d6e36342a83c31401079fa
Oliver Chang <oliverc1622@gmail.com>
2025-02-16 22:02:31 -0800

    preliminary env created, need to extend newtonian

--- parked_car_scenic_env.py
+++ parked_car_scenic_env.py
@@ -1,27 +1,27 @@
 # %%
 import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
 import scenic
 from simulator import NewtonianSimulator
+from scenic.domains.driving.actions import SetSteerAction, SetThrottleAction
+from scenic.domains.driving.roads import Network
 
 # %%
-scenario = scenic.scenarioFromFile('../Scenic-2.1.0/examples/gta/badlyParkedCar2.scenic',
-                                   model='scenic.simulators.newtonian.driving_model')
+network = Network.fromFile('maps/Town01.xodr')
+# Sample a new scenario from Scenic.
+scenario = scenic.scenarioFromFile(
+    'car.scenic',
+    model='scenic.simulators.newtonian.driving_model'
+)
 scene, _ = scenario.generate()
-simulator = NewtonianSimulator()
-simulation = simulator.simulate(scene, maxSteps=10)
-if simulation:  # `simulate` can return None if simulation fails
-    result = simulation.result
-    for i, state in enumerate(result.trajectory):
-            egoPos, parkedCarPos = state
-            print(f'Time step {i}: ego at {egoPos}; parked car at {parkedCarPos}')
-            scene.show(zoom=0.95)
-# %%
-
+simulator = NewtonianSimulator(network=network)
 simulation = simulator.createSimulation(scene, verbosity=0)
-simulation
 
 # %%
-simulation.objects
+SetThrottleAction(1.0).applyTo(scene.objects[0], simulator)
+simulation.step()
+simulation.objects[0].position
 
 
 # %% Gymnasium environment
@@ -36,12 +36,18 @@ class ScenicGymEnv(gym.Env):
         self.render_mode = render
         self.current_step = 0
 
-        # Define action space for the RL agent (e.g., throttle, steer, brake).
-        # TODO: define action space most likekly multi discrete/continuous
+        # just steer for now, throttle will be constant
+        self.action_space = spaces.Box(
+            low=np.array([-1.0]),  # steer
+            high=np.array([1.0]),
+            dtype=np.float32
+        )
 
-        # Define action space for the RL agent (e.g., throttle, steer, brake).
-        # TODO: define observation space.... 
-        
+        # [ego_x, ego_y, ego_heading].
+        self.observation_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32
+        )
+
         # Instantiate the simulator; you may pass a road network if available.
         self.simulator = NewtonianSimulator(timestep=self.timestep, render=self.render_mode)
         self.simulation = None
@@ -61,6 +67,42 @@ class ScenicGymEnv(gym.Env):
         # Create a new simulation from the scene.
         self.simulation = self.simulator.createSimulation(self.scene, verbosity=0)
         self.current_step = 0
-        
-        observation = self._get_observation()
+        ego = self.scene.objects[0]
+        observation = np.array([ego.position.x, ego.position.y, ego.heading], dtype=np.float32)
         return observation, {}
+    
+    def step(self, action):
+        # Apply the action to the simulation.
+        ego = self.scene.objects[0]
+        SetSteerAction(action).applyTo(ego, self.simulator)
+        SetThrottleAction(1.0).applyTo(ego, self.simulator) # constant for now
+        
+        self.simulation.step()
+        self.current_step += 1
+        observation = np.array([ego.position.x, ego.position.y, ego.heading], dtype=np.float32)
+        trunacted = self.current_step >= self.max_steps
+        reward = self._calculate_reward(observation)
+        terminated = reward < -20.0
+        return observation, reward, terminated, trunacted, {}
+
+
+    def _calculate_reward(self, observation):
+        lateral_error = (observation[1] - self.lane_center_y)**2
+        return -lateral_error
+    
+    def close(self):
+        pass
+    
+# %%
+env = ScenicGymEnv("../Scenic-2.1.0/examples/gta/badlyParkedCar2.scenic", render=True)
+
+# %%
+obs, _ = env.reset()
+print("Initial observation:", obs)
+done = False
+for i in range(10):
+    action = env.action_space.sample()  # Replace with agent's action in practice.
+    obs, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
+    print(f"Step {env.current_step}: Observation: {obs}, Reward: {reward}")
+env.close()
