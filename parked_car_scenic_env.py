b617ed520acf0cdfa2da5343247b48e0ffd67a20
Oliver Chang <oliverc1622@gmail.com>
2025-02-18 09:54:53 -0800

    step adjustments

--- parked_car_scenic_env.py
+++ parked_car_scenic_env.py
@@ -6,38 +6,15 @@ import scenic
 from simulator import NewtonianSimulator
 from scenic.domains.driving.actions import SetSteerAction, SetThrottleAction
 from scenic.domains.driving.roads import Network
-from stable_baselines3.common.env_checker import check_env
 
-# %%
-network = Network.fromFile('maps/Town01.xodr')
-# Sample a new scenario from Scenic.
-scenario = scenic.scenarioFromFile(
-    'car.scenic',
-    model='scenic.simulators.newtonian.driving_model'
-)
-scene, _ = scenario.generate()
-simulator = NewtonianSimulator(network=network, render=True)
-simulation = simulator.createSimulation(scene, verbosity=0)
-
-# %%
-img = scene.show()
-
-# %%
-
-simulation.step()
-scene.show(zoom=0.5)
-print(simulation.compute_lane_reward())
-print(f"ego position: {simulation.ego.position},\nprev position: {simulation.prev_ego_position}")
-
-SetThrottleAction(1.0).applyTo(scene.objects[0], simulator)
-SetSteerAction(-1.0).applyTo(scene.objects[0], simulator)
 
 # %% Gymnasium environment
 
 class ScenicGymEnv(gym.Env):
-    def __init__(self, scene_file: str, max_steps=100, timestep=0.1, render=False, seed=1):
+    def __init__(self, scene_file: str, network_file: str, max_steps=100, timestep=0.1, render=False, seed=1):
         super().__init__()
         self.scene_file = scene_file
+        self.network_file = network_file
         self.max_steps = max_steps
         self.timestep = timestep
         self.render_mode = render
@@ -55,9 +32,9 @@ class ScenicGymEnv(gym.Env):
         self.observation_space = spaces.Box(
             low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32
         )
-
+        self.network = Network.fromFile(self.network_file)
         # Instantiate the simulator; you may pass a road network if available.
-        self.simulator = NewtonianSimulator(timestep=self.timestep, render=self.render_mode)
+        self.simulator = NewtonianSimulator(network=self.network, timestep=self.timestep, render=self.render_mode)
         self.simulation = None
         self.scene = None
 
@@ -66,7 +43,7 @@ class ScenicGymEnv(gym.Env):
 
         # Sample a new scenario from Scenic.
         scenario = scenic.scenarioFromFile(
-            'car.scenic',
+            self.scene_file,
             model='scenic.simulators.newtonian.driving_model'
         )
         self.scene, _ = scenario.generate()
@@ -89,28 +66,13 @@ class ScenicGymEnv(gym.Env):
         observation = np.array([ego.position.x, ego.position.y, ego.heading], dtype=np.float32)
         trunacted = self.current_step >= self.max_steps
         reward = self.simulation.compute_lane_reward()
-        terminated = reward == -1
+        terminated = reward == -1 or trunacted
         return observation, reward, terminated, trunacted, {}
     
     def render(self):
-        scene.show()
+        self.scene.show()
 
     def close(self):
         pass
-    
-# %%
-env = ScenicGymEnv("car.scenic", render=False)
-check_env(env)
-
-# %%
-obs, _ = env.reset()
-print("Initial observation:", obs)
-done = False
-for i in range(10):
-    action = env.action_space.sample()  # Replace with agent's action in practice.
-    obs, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
-    print(f"Step {env.current_step}: Observation: {obs}, Reward: {reward}")
-env.close()
 
 # %%
