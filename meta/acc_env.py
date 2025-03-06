8fa65335c3f0b4d3a07d922c09c4dd622799c063
Oliver Chang <oliverc1622@gmail.com>
2025-03-06 08:57:16 +0000

    writing reward func - will prob need MARL

--- meta/acc_env.py
+++ meta/acc_env.py
@@ -5,6 +5,7 @@ class AccEnv(MetaDriveEnv):
     """Environment class for ACC theat model with configurable number of follower vehicles."""
     def __init__(self, config, ego_init_position: tuple[int,int], num_followers=2):
         super(AccEnv, self).__init__(config)
+        # TODO: Access the parameters in engine.global_config instead of storing them in the class
         self.ego_init_position = ego_init_position
         self.num_followers = num_followers
 
@@ -16,3 +17,7 @@ class AccEnv(MetaDriveEnv):
             AccTrafficManager(self.ego_init_position, num_followers=self.num_followers
         )
     ) 
+        
+    # def reward_function(*args, **kwargs):
+        
+    #     return reward, kwargs
