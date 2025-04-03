d680e9bb95e969e294ae42285f160b2bd9d139e1
Oliver Chang <oliverc1622@gmail.com>
2025-04-03 13:24:34 -0700

    model loading typo

--- acc_verifai/metadrive/inference.py
+++ acc_verifai/metadrive/inference.py
@@ -10,18 +10,20 @@ from stable_baselines3 import PPO
 from stable_baselines3.common import results_plotter
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.utils import set_random_seed
 
 
 # %%
-
 results_plotter.plot_results(
-    ["logs/tmp6"],
+    ["logs/tmp10"],
     num_timesteps=100_000,
     x_axis=results_plotter.X_TIMESTEPS,
     task_name="PPO IDM Attacker",
 )
 
 # %%
+
+set_random_seed(0)
 
 scenario = scenic.scenarioFromFile(
         "idm.scenic",
@@ -31,21 +33,21 @@ scenario = scenic.scenarioFromFile(
 
 env = ScenicGymEnv(
     scenario,
-    MetaDriveSimulator(timestep=0.1, sumo_map=pathlib.Path("../maps/Town06.net.xml"), render=True, real_time=True),
+    MetaDriveSimulator(timestep=0.05, sumo_map=pathlib.Path("../maps/Town06.net.xml"), render=True, real_time=False),
     observation_space=spaces.Box(low=-np.inf, high=np.inf, shape=(4,4)),
     action_space=spaces.Box(low=-1, high=1, shape=(1,)),
-    max_steps=300,
+    max_steps=600,
 )
 
 log_dir="logs/inference"
-env = Monitor(env, log_dir, info_keywords=("attacker_crashed", "counter_example_found", "timeout", "dense_reward_signals"))
+env = Monitor(env, log_dir) # , info_keywords=("attacker_crashed", "counter_example_found", "dense_reward_signals")
 
 
 # %%
-model = PPO("MlpPolicy", env, verbose=1)
-model.load("models/ppo_idm_attacker5", env=env)
+model = PPO.load("models/ppo_idm_attacker10", env=env)
 
 # %%
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
+print(f"Mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
 
 # %%
