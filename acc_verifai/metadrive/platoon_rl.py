b8eb8e9171b285766d68e92d3a5778b373f8978d
Oliver Chang <oliverc1622@gmail.com>
2025-04-02 17:32:33 -0700

    default timestep

--- acc_verifai/metadrive/platoon_rl.py
+++ acc_verifai/metadrive/platoon_rl.py
@@ -22,20 +22,20 @@ def main() -> None:
         mode2D=True,
     )
 
-    log_dir="tmp2/"
+    log_dir="logs/tmp6/"
 
     env = ScenicGymEnv(
         scenario,
-        MetaDriveSimulator(sumo_map=pathlib.Path("../maps/Town06.net.xml"), render=False, real_time=False),
+        MetaDriveSimulator(timestep=0.1, sumo_map=pathlib.Path("../maps/Town06.net.xml"), render=False, real_time=False),
         observation_space=spaces.Box(low=-np.inf, high=np.inf, shape=(4,4)),
         action_space=spaces.Box(low=-1, high=1, shape=(1,)),
         max_steps=300,
     )
-    env = Monitor(env, log_dir)
+    env = Monitor(env, log_dir, info_keywords=("attacker_crashed", "counter_example_found", "timeout", "dense_reward_signals"))
 
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=100_000, progress_bar=True)
-    model.save("ppo_idm_attacker2")
+    model.save("ppo_idm_attacker6")
 
 if __name__== "__main__":
     main()
