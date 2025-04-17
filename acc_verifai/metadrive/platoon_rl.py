bca61acc7756e83bbde983e19b1c5745d7fbec72
Oliver Chang <oliverc1622@gmail.com>
2025-04-16 18:28:31 -0700

    adjusted for idm mobil falsification

--- acc_verifai/metadrive/platoon_rl.py
+++ acc_verifai/metadrive/platoon_rl.py
@@ -2,13 +2,14 @@
 import pathlib
 
 import numpy as np
-import scenic
 from gymnasium import spaces
-from scenic.gym import ScenicGymEnv
-from scenic.simulators.metadrive import MetaDriveSimulator
 from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.utils import set_random_seed
+
+import scenic
+from scenic.gym import ScenicGymEnv
+from scenic.simulators.metadrive import MetaDriveSimulator
 
 
 # %%
@@ -27,15 +28,15 @@ def main() -> None:
     env = ScenicGymEnv(
         scenario,
         MetaDriveSimulator(timestep=0.1, sumo_map=pathlib.Path("../maps/Town06.net.xml"), render=False, real_time=False),
-        observation_space=spaces.Box(low=-np.inf, high=np.inf, shape=(4,4)),
-        action_space=spaces.Box(low=-1, high=1, shape=(1,)),
-        max_steps=300,
+        observation_space=spaces.Box(low=-np.inf, high=np.inf, shape=(5,4)),
+        action_space=spaces.Box(low=-1, high=1, shape=(2,)),
+        max_steps=700,
     )
-    env = Monitor(env, log_dir, info_keywords=("attacker_crashed", "counter_example_found", "timeout", "dense_reward_signals"))
+    env = Monitor(env, log_dir, info_keywords=("attacker_crashed", "counter_example_found", "dense_reward_signals"))
 
     model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=100_000, progress_bar=True)
-    model.save("ppo_idm_attacker6")
+    model.learn(total_timesteps=200_000, progress_bar=True)
+    model.save("models/ppo_idm_attacker6")
 
 if __name__== "__main__":
     main()
